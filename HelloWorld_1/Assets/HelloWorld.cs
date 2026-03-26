84fa6a496147599509170058138afafa3af9f112
acliang1412 <acliang7229@gmail.com>
2026-03-25 22:34:07 -0500

    File changed
    Version control

---
+++ HelloWorld_1/Assets/HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This runs once when the game starts
+        print("Hello my name is Andy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This runs over and over again (perfect for spinning!)
+    }
+}
