12943dd1b8aa89d164349b4431179e09b38b2906
acliang1412 <acliang7229@gmail.com>
2026-03-26 18:50:21 -0500

    Unity, player inputs
    added new basic codes that will allow to recieve play inputs

--- Magic Game Cube/Assets/Cube.cs
+++ Magic Game Cube/Assets/Cube.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    int numberOfTimes = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("If you press the up arrow you'lll jump!");
-        Debug.Log("If you press the down arrow you'lll crouch!");
+        //Debug.Log("If you press the up arrow you'll jump!");
+        //Debug.Log("If you press the down arrow " + numberOfTimes + " you'll crouch!");
 
-        Debug.LogWarning("if you press space, nothing happens");
-        Debug.LogError("If you smash the keyboard nothing happens, you just cry");
+        //Debug.LogWarning("if you press space, nothing happens");
+        //Debug.LogError("If you smash the keyboard nothing happens, you just cry");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("You pressed space");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("You pressed Left");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("You pressed right");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("You pressed down");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("You pressed up");
+        }
     }
 }
