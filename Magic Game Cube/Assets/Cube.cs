cc50b8f59eb27bcec41f76f64e5924bea4005554
acliang1412 <acliang7229@gmail.com>
2026-03-26 20:05:42 -0500

    Public methods and return types
    learning to passed in methods in unity

--- Magic Game Cube/Assets/Cube.cs
+++ Magic Game Cube/Assets/Cube.cs
@@ -11,15 +11,45 @@ public class Cube : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Debug.Log("If you press the up arrow you'll jump!");
-        //Debug.Log("If you press the down arrow " + numberOfTimes + " you'll crouch!");
-
-        //Debug.LogWarning("if you press space, nothing happens");
-        //Debug.LogError("If you smash the keyboard nothing happens, you just cry");
+        //PrintingToOurConsole();
     }
+
+    
 
     // Update is called once per frame
     void Update()
+    {
+        MovingOurCube();
+        OutOfBoundsPrinter();
+    }
+
+    public string PrintingFromOutside(int value)
+    {
+        string printSomething = "The value you passed in was " + value; 
+        return printSomething;
+    }
+
+    private void OutOfBoundsPrinter()
+    {
+        if (transform.position.x > 12.5f)
+        {
+            Debug.LogWarning("You went too far to the right!");
+        }
+        else if (transform.position.x < -12.5f)
+        {
+            Debug.LogWarning("You went too far to the left!");
+        }
+        else if (transform.position.y > 6.5f)
+        {
+            Debug.LogWarning("You went too far up!");
+        }
+        else if (transform.position.y < -6.5f)
+        {
+            Debug.LogWarning("You went too far down!");
+        }
+    }
+
+    private void MovingOurCube()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -41,22 +71,14 @@ public class Cube : MonoBehaviour
         {
             myRigidBody2D.linearVelocity = new Vector2(0f, 10f);
         }
+    }
 
-        if (transform.position.x > 12.5f)
-        {
-            Debug.LogWarning("You went too far to the right!");
-        }
-        else if (transform.position.x < -12.5f)
-        {
-            Debug.LogWarning("You went too far to the left!");
-        }
-        else if (transform.position.y > 6.5f)
-        {
-            Debug.LogWarning("You went too far up!");
-        }
-        else if (transform.position.y < -6.5f)
-        {
-            Debug.LogWarning("You went too far down!");
-        }
+    void PrintingToOurConsole()
+    {
+        Debug.Log("If you press the up arrow you'll jump!");
+        Debug.Log("If you press the down arrow " + numberOfTimes + " you'll crouch!");
+
+        Debug.LogWarning("if you press space, nothing happens");
+        Debug.LogError("If you smash the keyboard nothing happens, you just cry");
     }
 }
